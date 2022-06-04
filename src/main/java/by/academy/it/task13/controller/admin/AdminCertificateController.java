954ade689a31f7ca1117ae95c34b36ac3118f139
extensaman <verus.wedding@gmail.com>
2022-06-04 11:55:25 +0300

    Select draft works

--- src/main/java/by/academy/it/task13/controller/admin/AdminCertificateController.java
+++ src/main/java/by/academy/it/task13/controller/admin/AdminCertificateController.java
@@ -2,6 +2,7 @@ package by.academy.it.task13.controller.admin;
 
 import by.academy.it.task13.entity.Certificate;
 import by.academy.it.task13.service.CertificateService;
+import by.academy.it.task13.service.CertificateTypeService;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -21,17 +22,21 @@ public class AdminCertificateController {
 
     @Autowired
     private final CertificateService certificateService;
+    @Autowired
+    private final CertificateTypeService certificateTypeService;
 
     @GetMapping
     public String getGiftCertificatePage(Model model) {
         model.addAttribute(AdminConstant.TITLE,
                 AdminConstant.MENU_ADMIN_GIFT_CERTIFICATE_MESSAGE);
         model.addAttribute(AdminConstant.GIFT_CERTIFICATE_LIST, certificateService.findAll());
+        model.addAttribute(AdminConstant.CERTIFICATE_TYPE_LIST, certificateTypeService.findAll());
         return AdminConstant.ADMIN_GIFT_CERTIFICATE_PAGE;
     }
 
     @PostMapping
     public String saveGiftCertificateChange(@ModelAttribute Certificate certificate) {
+        LOGGER.info("CertificatType = " + certificate.getCertificateType());
         certificateService.save(certificate);
         return "redirect:/admin/certificate";
     }
