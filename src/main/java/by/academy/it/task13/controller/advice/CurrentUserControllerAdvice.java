35642ec3cd5c3382710051c884062fe7e3508a63
extensaman <verus.wedding@gmail.com>
2022-06-20 19:57:40 +0300

    UserNameDto added to @ControllerAdvice

--- src/main/java/by/academy/it/task13/controller/advice/CurrentUserControllerAdvice.java
+++ src/main/java/by/academy/it/task13/controller/advice/CurrentUserControllerAdvice.java
@@ -1,6 +1,9 @@
 package by.academy.it.task13.controller.advice;
 
+import by.academy.it.task13.dto.user.UserNameDto;
 import by.academy.it.task13.entity.User;
+import by.academy.it.task13.mapper.impl.UserNameMapper;
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.security.core.Authentication;
@@ -8,16 +11,18 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
+@RequiredArgsConstructor
 public class CurrentUserControllerAdvice {
     private static final Logger LOGGER = LogManager.getLogger(CurrentUserControllerAdvice.class);
+    private final UserNameMapper mapper;
 
     @ModelAttribute("currentUser")
-    public User getCurrentUser(Authentication authentication) {
+    public UserNameDto getCurrentUser(Authentication authentication) {
         User user = null;
         if (authentication != null) {
             user = (User) authentication.getPrincipal();
             LOGGER.info(user.getUsername());
         }
-        return user;
+        return mapper.toDto(user);
     }
 }
