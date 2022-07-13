775bda5eeeb977fcd915a20a066db3d6a55bea0c
extensaman <verus.wedding@gmail.com>
2022-07-13 23:57:27 +0300

    Made equal menus in Thymeleaf and Angular

--- AngularFrontend/src/app/view/header/header.component.ts
+++ AngularFrontend/src/app/view/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AppSetting} from "../../setting/app-setting";
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  backendUrl: string = AppSetting.backendUrl;
 
   constructor() { }
 
